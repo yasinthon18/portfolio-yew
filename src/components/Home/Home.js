0954e4ace035761d672e2df6dbee4a8bf726ae95
yasinthon18 <s6603052423031@email.kmutnb.ac.th>
2025-03-25 18:53:15 +0700

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> SUKRUT RAHANE </strong>
+                <strong className="main-name"> Yasinthon Siwarat </strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
