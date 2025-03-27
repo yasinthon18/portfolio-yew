163ed6e0f7ac77fe51da72f85b345049d815dbc9
yasinthon18 <s6603052423031@email.kmutnb.ac.th>
2025-03-27 13:58:24 +0700

    latest update

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { ImPacman, ImPointRight } from "react-icons/im";
+import { ImPacman } from "react-icons/im";
 
 function AboutCard() {
   return (
